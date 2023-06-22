69037cab4190514b4395ac90ad01cbd2fd709f62
Eshal526 <131014696+Eshal526@users.noreply.github.com>
2023-06-23 01:33:25 +0500

    main page

--- app/src/main/java/com/example/sabaq/MainActivity5.java
+++ app/src/main/java/com/example/sabaq/MainActivity5.java
@@ -1,31 +1,67 @@
 package com.example.sabaq;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity5 extends AppCompatActivity {
 
-    ListView list2;
+    private ListView listView;
+    private EditText searchEditText;
+    private ArrayAdapter<Student> arrayAdapter;
+    Student student;
+    private List<Student> studentList;
+    private DbHelper dbHelper;
 
-    DbHelper db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
 
-        list2 = findViewById(R.id.list2);
-        db = new DbHelper(this);
+        listView = findViewById(R.id.list2);
+        searchEditText = findViewById(R.id.searchEditText);
+        dbHelper = new DbHelper(this);
+        studentList = new ArrayList<>();
 
-        ArrayAdapter<Records> arrayAdapter = new ArrayAdapter<Records>(this , android.R.layout.simple_list_item_1 , db.selectAllRecords());
-        list2.setAdapter(arrayAdapter);
+        // Set up the ArrayAdapter with the initial student list
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, studentList);
+        listView.setAdapter(arrayAdapter);
 
+        // Set up the text watcher for search functionality
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Not needed for this implementation
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Perform the search whenever the text changes
+                searchStudents(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Not needed for this implementation
+            }
+        });
+    }
+
+    private void searchStudents(String searchQuery) {
+        studentList.clear();
+        studentList.addAll(dbHelper.searchStudents(searchQuery));
+        arrayAdapter.notifyDataSetChanged();
     }
 }
