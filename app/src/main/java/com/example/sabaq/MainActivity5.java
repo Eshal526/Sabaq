2254532d85037db4667a4f93662c16b447b5d69e
Eshal526 <131014696+Eshal526@users.noreply.github.com>
2023-06-23 02:08:06 +0500

    main page

--- app/src/main/java/com/example/sabaq/MainActivity5.java
+++ app/src/main/java/com/example/sabaq/MainActivity5.java
@@ -3,12 +3,16 @@ package com.example.sabaq;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+//import android.view.View;
 import android.widget.ArrayAdapter;
+//import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+//import android.widget.Toast;
 
 
 import java.util.ArrayList;
@@ -18,6 +22,8 @@ public class MainActivity5 extends AppCompatActivity {
 
     private ListView listView;
     private EditText searchEditText;
+
+  //  private Button button8;
     private ArrayAdapter<Student> arrayAdapter;
     Student student;
     private List<Student> studentList;
@@ -63,5 +69,19 @@ public class MainActivity5 extends AppCompatActivity {
         studentList.clear();
         studentList.addAll(dbHelper.searchStudents(searchQuery));
         arrayAdapter.notifyDataSetChanged();
+
+       /* button8=findViewById(R.id.addStudent);
+
+        DbHelper db = DbHelper.getReadableDatabase(this);
+        // SQLiteDatabase db = dbHelper.getWritableDatabase();
+        //   db = new DbHelper(this);
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity5.this, MainActivity4.class);
+                // intent.putExtra("db", (Serializable) db);
+                startActivity(intent);
+            }
+        });*/
     }
 }
